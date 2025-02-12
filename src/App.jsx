f9d05aa868615851e4238332ac55629fc8f46d65
Matan Yeshurun <matan.yeshurun@otorio.com>
2025-02-12 11:54:13 +0200

    styled bug fixes

--- src/App.jsx
+++ src/App.jsx
@@ -5,38 +5,44 @@ import './App.css'
 import Clock from './components/Clock'
 import styled from 'styled-components'
 import bgImage from '/Users/matanyes/personal-dev/fittr/src/fittr_transparent.png';
+import { ThemeProvider } from './theme/theme'
+
+export const Background = styled.div`
+display: flex;
+flex-direction: column;
+min-width: 100vw;
+min-height: 100vh;
+background-image: url(${props => props.bg$});
+background-size: 55% 55%;
+background-position: top center;
+background-repeat: no-repeat;
+background-color: ${(props) => props.theme.background};
+`;
+
+export const Content = styled.div`
+  position: absolute;
+  top: 40%; /* Starts at 70% from the top */
+  left: 50%;
+  transform: translate(-50%, 0); /* Centers horizontally */
+  width: 80%; /* Adjust width as needed */
+  color: white;
+  padding: 20px;
+  text-align: center;
+  border-radius: 10px;
+`;
 
 function App() {
   
-  const Background = styled.div`
-  display: flex;
-  flex-direction: column;
-  min-width: 100vw;
-  min-height: 100vh;
-  background-image: url(${props => props.bg});
-  background-size: 55% 55%;
-  background-position: top center;
-  background-repeat: no-repeat;
-`;
 
-const Content = styled.div`
-    position: absolute;
-    top: 40%; /* Starts at 70% from the top */
-    left: 50%;
-    transform: translate(-50%, 0); /* Centers horizontally */
-    width: 80%; /* Adjust width as needed */
-    color: white;
-    padding: 20px;
-    text-align: center;
-    border-radius: 10px;
-`;
   return (
     <>
+    <ThemeProvider>
       <Background bg={bgImage}> 
         <Content>
-          <Clock />
+          <Clock/>
         </Content>
       </Background>
+    </ThemeProvider>
     </>
   )
 }
--- src/components/Clock.jsx
+++ src/components/Clock.jsx
@@ -1,8 +1,47 @@
 import React, {useState, useEffect} from "react"
 import styled from "styled-components";
+import { useTheme } from "../theme/theme";
+
+
+
+export const ClockElement = styled.span`
+    font-family: 'Montserrat', sans-serif;
+    font-size: 14rem;
+    font-weight: bold;
+    text-align: center;
+    color: #d34444;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    
+    text-shadow: 
+        -2px -2px 0 black,  
+        2px -2px 0 black,
+        -2px  2px 0 black,
+        2px  2px 0 black;
+
+`;
+export const DateElement = styled(ClockElement)`
+    font-size: 4rem;
+
+`;
 
 export default function Clock(){
     const [currentTime, setCurrentTime] = useState(new Date());
+
+    const formattedTime = currentTime.toLocaleString("en-US", {
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit",
+        hour12: false, // Set to false for 24-hour format, true for 12-hour format
+    });
+    
+    const formattedDate = currentTime.toLocaleDateString("en-US", {
+        weekday: "short",
+        month: "short",
+        day: "numeric",
+        year: "numeric",
+    })
 
     useEffect( () => {
         const interval = setInterval(()=> {
@@ -11,47 +50,13 @@ export default function Clock(){
         return () => clearInterval(interval);
     }, []);
 
-    const formattedTime = currentTime.toLocaleString("en-US", {
-        hour: "2-digit",
-        minute: "2-digit",
-        second: "2-digit",
-        hour12: false, // Set to false for 24-hour format, true for 12-hour format
-    });
-
-    const formattedDate = currentTime.toLocaleDateString("en-US", {
-        weekday: "short",
-        month: "short",
-        day: "numeric",
-        year: "numeric",
-    })
-
-    const ClockElement = styled.span`
-        font-family: 'Montserrat', sans-serif;
-        font-size: 14rem;
-        font-weight: bold;
-        text-align: center;
-        color: #d34444;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        
-        text-shadow: 
-            -2px -2px 0 black,  
-            2px -2px 0 black,
-            -2px  2px 0 black,
-            2px  2px 0 black;
-    
-    `;
-    const DateElement = styled(ClockElement)`
-        font-size: 4rem;
-    
-    `;
 
 
+    const { toggleTheme } = useTheme();
     return (
         <>
             <ClockElement> {formattedTime}</ClockElement>
-            <DateElement> {formattedDate} </DateElement><br />
+            <DateElement onClick={toggleTheme}> {formattedDate} </DateElement><br />
         </>
     )
 }
